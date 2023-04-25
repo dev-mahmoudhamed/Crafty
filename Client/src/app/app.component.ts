4cd98ae5efd80e3534873b1fdde66afae52226e3
Mahmoud Hamed <dev.mahmoudhamed@gmail.com>
2023-04-25 18:24:13 +0200

    Add Product interface to angular

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/products';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Crafty';
+  products?: Product[];
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    let url = "https://localhost:7007/api/products?pageSize=50";
+
+    this.http.get<Pagination<Product[]>>(url).subscribe({
+      next: (response) => this.products = response.data,
+      error: (err) => console.log(err)
+    });
+  }
 }
