c54f5f4c7323fdf542c00bf6f32d2bf2868705cc
Mahmoud Hamed <dev.mahmoudhamed@gmail.com>
2023-05-03 12:21:49 +0300

    Improve UI

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/products';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -10,7 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -19,7 +23,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) this.shopService.getProduct(+id).subscribe({
-      next: productReturned => this.product = productReturned,
+      next: productReturned => {
+        this.product = productReturned;
+        this.bcService.set('@productDetails', productReturned.name)
+      },
       error: err => console.log(err)
     });
   }
